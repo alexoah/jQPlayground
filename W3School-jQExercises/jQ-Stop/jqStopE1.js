ce998bc5294092276825333c2d15ab8714e6e819
Alexoah YT <alexandra.soah@gmail.com>
2020-04-13 16:36:33 +0700

    added answer files for jQuery Stop exercise

---
+++ W3School-jQExercises/jQ-Stop/jqStopE1.js
@@ -0,0 +1,10 @@
+/**
+ *	from jQuery Stop: Exercise 1 ( https://www.w3schools.com/jquery/exercise_jq.asp?filename=exercise_jq_stop1 )
+ *
+ *	question:
+ *	Use a jQuery method to stop the animation effect of a <div> element.
+ *
+ *	$("div").____();
+ *
+ */
+$("div").stop();
