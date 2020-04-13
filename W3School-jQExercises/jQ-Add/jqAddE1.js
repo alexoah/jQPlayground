89950c383007f48f661733c4d36af5360cba0652
Alexoah YT <alexandra.soah@gmail.com>
2020-04-13 16:37:01 +0700

    added answer files for jQuery Add exercises

---
+++ W3School-jQExercises/jQ-Add/jqAddE1.js
@@ -0,0 +1,10 @@
+/**
+ *	from jQuery Add: Exercise 1 ( https://www.w3schools.com/jquery/exercise_jq.asp?filename=exercise_jq_dom_add1 )
+ *
+ *	question:
+ *	Use a jQuery method to insert the text "YES!" at the end of a <p> element.
+ *
+ *	$("p").______("YES!");
+ *
+ */
+$("p").append("YES!");
---
+++ W3School-jQExercises/jQ-Add/jqAddE2.js
@@ -0,0 +1,10 @@
+/**
+ *	from jQuery Add: Exercise 2 ( https://www.w3schools.com/jquery/exercise_jq.asp?filename=exercise_jq_dom_add2 )
+ *
+ *	question:
+ *	Use a jQuery method to insert the text "YES!" at the beginning of a <p> element.
+ *
+ *	$("p")._______("YES!");
+ *
+ */
+$("p").prepend("YES!");
